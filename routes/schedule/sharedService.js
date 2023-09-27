0a9604ca93d3fa8173d8aeb92c2b54bc4fd18fa9
Rajesh Kumar Ranjan <rajesh.ranjan@lntedutech.com>
2023-09-27 17:47:04 +0530

    Update sharedService.js

--- routes/schedule/sharedService.js
+++ routes/schedule/sharedService.js
@@ -20,15 +20,20 @@ let tokenValidation = async(params)=> {
             decodedToken.headers = params.body.authorization;
             let username = decodedToken.username.replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi,'_');
             if(decodedToken){
+                console.log('decodedToken 1')
                 let userResponse = await scheduleService.userFetch(decodedToken);
                 var responseData ;				
                 if (userResponse&&userResponse.message&&(userResponse.message.length>0) &&(userResponse.message[0]._id == username)){
+                    console.log('userResponse.message.length 2')
                         let response = await scheduleService.userUpdate(userResponse.message[0]);
                         if (response && response.success){
+                            console.log('response.success 3')
                             let roomsResponse = await scheduleService.roomFetch(decodedToken);
                             if (roomsResponse && roomsResponse.success && (roomsResponse.message.length>0) && (roomsResponse.message[0]._id == decodedToken.id)){
+                                console.log('roomsResponse.success 4')
                                 responseData = await scheduleService.roomUpdate(decodedToken)
                             } else{
+                                console.log('roomsResponse.message.length 5')
                                 responseData = await scheduleService.roomInsertion(decodedToken);
                             }
                             
