6d9adfaef344f47cfb4dfc17b804ea909e56169d
Rajesh Kumar Ranjan <rajesh.ranjan@lntedutech.com>
2023-10-24 14:15:57 +0530

    Update scheduleService.js

--- routes/schedule/scheduleService.js
+++ routes/schedule/scheduleService.js
@@ -126,6 +126,7 @@ let userFetch = async (params) => {
             query: username
         };
         let responseData = await invoke.makeHttpCall("post", "findById", getdata);
+        console.log(responseData.data,'responseData.data')
         if (responseData && responseData.data && responseData.data.statusMessage) {
             return { success: true, message:responseData.data.statusMessage}
         } else {
