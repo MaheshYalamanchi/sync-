ad5e40b313591465ddd2305823e90a94cf3b68f1
mahesh <mahesh.yalamnchi@lntecc.com>
2023-11-08 13:10:42 +0530

    Update sharedService.js

--- routes/schedule/sharedService.js
+++ routes/schedule/sharedService.js
@@ -109,7 +109,7 @@ let validateToken = async(params)=> {
                                     responseData = await scheduleService.roomInsertion(decodedToken);
                                  }
                         } else {
-                            console.log("userresponse112======>",JSON.stringify(response.message.response.data))
+                            console.log("userresponse112======>",JSON.stringify(response.message.response))
                         }
                     }else{
                         return {success:false, message : 'Data Not Found'};
