831634279be61fa0e72a80bd6dceeaf513672986
mahesh <mahesh.yalamnchi@lntecc.com>
2023-11-15 14:26:22 +0530

    connection close cron job is commented

--- app.js
+++ app.js
@@ -47,15 +47,15 @@ var CronJob = require('cron').CronJob;
 // }, null, true, "Asia/Calcutta");
 
 // cronjob for every  15 secs.
-new CronJob('*/15 * * * * *', function() {
-    request(process.env.CLOSE_CONEECTION_ENDPOINT, function(error, response, body) {
-        if (!error && response.statusCode == 200) {
-          // console.log('You will see this message every 2 minutes');
-        } else {
-          console.log(error)
-        }
-    })
-}, null, true, "Asia/Calcutta");
+// new CronJob('*/15 * * * * *', function() {
+//     request(process.env.CLOSE_CONEECTION_ENDPOINT, function(error, response, body) {
+//         if (!error && response.statusCode == 200) {
+//           // console.log('You will see this message every 2 minutes');
+//         } else {
+//           console.log(error)
+//         }
+//     })
+// }, null, true, "Asia/Calcutta");
 
 app.use(function(req, res, next) {
   next(createError(404));
