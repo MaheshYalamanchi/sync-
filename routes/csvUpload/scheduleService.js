9f5a196cec8bdf0050feb959a1eb7eff4672722a
mahesh <mahesh.yalamnchi@lntecc.com>
2024-08-05 18:31:23 +0530

    #75580

--- routes/csvUpload/scheduleService.js
+++ routes/csvUpload/scheduleService.js
@@ -101,6 +101,7 @@ let getSessions = async (params) => {
   try {
       let fetchTenantResponse = await _schedule.fetchTenant();
       if(fetchTenantResponse && fetchTenantResponse.success){
+        console.log("===========>>>>>>1",fetchTenantResponse)
           fetchTenantResponse.message.forEach(async element => {
               var getdata = {
                   url: element?.connectionString+'/'+element?.databaseName,
@@ -129,8 +130,9 @@ let getSessions = async (params) => {
                   return { success: false, message: 'Status not updated...' };
               }
           });
-      }
-      var getdata = {
+      } else {
+        console.log("===========>>>>>>2",process.env.MONGO_URI+'/'+process.env.DATABASENAME)
+        var getdata = {
           url: process.env.MONGO_URI+'/'+process.env.DATABASENAME,
           database: process.env.DATABASENAME,
           model: "rooms",
@@ -139,22 +141,23 @@ let getSessions = async (params) => {
               filter:{ complete: { $ne: !0 }, status: "started", updatedAt: { $lt: new Date(Date.now() - 12e4) } },
               update:{$set:{ status: "paused"}}
           }   
-      };
-      // let response = await schedule.fetchdata(getdata)
-      let responseData = await invoke.makeHttpCall("post", "updatedataMany", getdata);
-      if(responseData && responseData.data && responseData.data.statusMessage.nModified>0) {
-          // let closeconnection = await invoke.makeHttpCall("get", "closeconnection");
-          // for (const iterator of response.data.statusMessage) {
-          //     let jsondata = {
-          //         pausetime : new Date(),
-          //         room : iterator._id
-          //     }
-          //     let reportlog = await invoke.makeHttpCalluserservice("post", "/api/reportlog", jsondata);
-          // }
-          return { success: true, message: 'Status updated successfully...' };
-      } else {
-          // let closeconnection = await invoke.makeHttpCall("get", "closeconnection");
-          return { success: false, message: 'Status not updated...' };
+        };
+        // let response = await schedule.fetchdata(getdata)
+        let responseData = await invoke.makeHttpCall("post", "updatedataMany", getdata);
+        if(responseData && responseData.data && responseData.data.statusMessage.nModified>0) {
+            // let closeconnection = await invoke.makeHttpCall("get", "closeconnection");
+            // for (const iterator of response.data.statusMessage) {
+            //     let jsondata = {
+            //         pausetime : new Date(),
+            //         room : iterator._id
+            //     }
+            //     let reportlog = await invoke.makeHttpCalluserservice("post", "/api/reportlog", jsondata);
+            // }
+            return { success: true, message: 'Status updated successfully...' };
+        } else {
+            // let closeconnection = await invoke.makeHttpCall("get", "closeconnection");
+            return { success: false, message: 'Status not updated...' };
+        }
       }
   } catch (error) {
       if (error && error.code == 'ECONNREFUSED') {
