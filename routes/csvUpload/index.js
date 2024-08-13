9fa01240b3c728999ebe56df307923807188868b
Rajesh Kumar Ranjan <rajesh.ranjan@lntedutech.com>
2024-08-13 12:41:34 +0000

    Updated index.js

--- routes/csvUpload/index.js
+++ routes/csvUpload/index.js
@@ -129,6 +129,7 @@ app.get('/api/sessions/stopped', async (req, res, next) => {
         }
       ]
       var responseData = await schedule.aggregate(data);
+      console.log(responseData)
       if (responseData && responseData.length > 0) {
         let Bulkdata = []
         for (const iterator of responseData) {
