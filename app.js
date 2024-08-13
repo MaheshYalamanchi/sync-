886bec7b9e4a506c5d5c30f924635dff4cb05577
mahesh <mahesh.yalamnchi@lntecc.com>
2024-08-13 12:02:57 +0530

    #76040
    Large payload   error handling

--- app.js
+++ app.js
@@ -81,6 +81,9 @@ app.use(function (req, res, next) {
   next(createError(404));
 });
 
+app.use(express.json({ limit: '50mb' })); // Adjust the limit as needed
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
 // error handler
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
