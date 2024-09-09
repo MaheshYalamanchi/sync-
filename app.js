e86b3c60f4f87bc4cf8620dbad045bfbb3742240
Mahesh Yalamanchi <mahesh.yalamanchi@lntecc.com>
2024-09-09 06:21:30 +0000

    Updated app.js

--- app.js
+++ app.js
@@ -39,33 +39,33 @@ require('./routes/tenant/index')({ app: app })
 var request = require('request')
 var CronJob = require('cron').CronJob;
 var PauseCronJob = require('cron').CronJob;
-// new CronJob('*/3 * * * *', function () {
-//   try {
-//     request(process.env.STOP_ENDPOINT, function (error, response, body) {
-//       if (!error && response.statusCode == 200) {
-//         console.log('You will see this message every 3 minutes(stop)');
-//       } else {
-//         console.log("StopErrorLog====>>>>", error)
-//       }
-//     })
-//   } catch (error) {
-//     console.log("StopCatchLog====>>>>", error)
-//   }
-// }, null, true, "Asia/Calcutta");
+new CronJob('*/3 * * * *', function () {
+  try {
+    request(process.env.STOP_ENDPOINT, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        console.log('You will see this message every 3 minutes(stop)');
+      } else {
+        console.log("StopErrorLog====>>>>", error)
+      }
+    })
+  } catch (error) {
+    console.log("StopCatchLog====>>>>", error)
+  }
+}, null, true, "Asia/Calcutta");
 
-// new PauseCronJob('*/2 * * * *', function () {
-//   try {
-//     request(process.env.PAUSE_ENDPOINT, function (error, response, body) {
-//       if (!error && response.statusCode == 200) {
-//         console.log('You will see this message every 2 minutes(pause)');
-//       } else {
-//         console.log("PauseErrorLog====>>>>", error)
-//       }
-//     })
-//   } catch (error) {
-//     console.log("PauseCatchLog====>>>>", error)
-//   }
-// }, null, true, "Asia/Calcutta");
+new PauseCronJob('*/2 * * * *', function () {
+  try {
+    request(process.env.PAUSE_ENDPOINT, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        console.log('You will see this message every 2 minutes(pause)');
+      } else {
+        console.log("PauseErrorLog====>>>>", error)
+      }
+    })
+  } catch (error) {
+    console.log("PauseCatchLog====>>>>", error)
+  }
+}, null, true, "Asia/Calcutta");
 
 // cronjob for every  15 secs.
 // new CronJob('*/15 * * * * *', function() {
