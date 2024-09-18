b92678ba71e3e2eff449d5c485455fe3281ad404
Rajesh Kumar Ranjan <rajesh.ranjan@lntecc.com>
2024-09-18 16:26:04 +0530

    pointing to proctor dataservice

--- routes/schedule/scheduleService.js
+++ routes/schedule/scheduleService.js
@@ -198,7 +198,7 @@ let userUpdate = async (params) => {
                 update: { $set: jsonData }
             }
         };
-        let responseData = await invoke.makeHttpCall_userDataService("post", "findOneAndUpdate", getdata);
+        let responseData = await invoke.makeHttpCall("post", "findOneAndUpdate", getdata);
         if (responseData && responseData.data && responseData.data.statusMessage) {
             return { success: true, message: responseData.data.statusMessage}
         } else {
